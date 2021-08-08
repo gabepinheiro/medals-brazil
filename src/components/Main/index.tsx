b7ca17e81e4615249e2291eb3cac252cc72a5111
Gabriel Pinheiro <gabrielpsilvas@live.com>
2021-08-08 20:33:21 -0300

    Rename project and update boilerplate initial

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,21 +1,8 @@
-import * as S from './styles'
-
-const Main = ({
-  title = 'React Avançado',
-  description = 'TypeScript, ReactJS, NextJS e Styled Components'
-}) => (
-  <S.Wrapper>
-    <S.Logo
-      src="/img/logo.svg"
-      alt="Imagem de um átomo e React Avançado escrito ao lado."
-    />
-    <S.Title>{title}</S.Title>
-    <S.Description>{description}</S.Description>
-    <S.Illustration
-      src="/img/hero-illustration.svg"
-      alt="Um desenvolvedor de frente para uma tela com código."
-    />
-  </S.Wrapper>
+const Main = () => (
+  <div>
+    <h1>My Trips</h1>
+    <a href="#">Link</a>
+  </div>
 )
 
 export default Main
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,14 +7,14 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>My Trips</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="A simple project to show my favorite spots in the world"
         />
       </Head>
       <GlobalStyles />
