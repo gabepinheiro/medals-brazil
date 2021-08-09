ae0c80a891df18aa83892bf7f7d3c5ce71d5ad3c
Gabriel Pinheiro <gabrielpsilvas@live.com>
2021-08-09 17:02:19 -0300

    Create generic PageTemplate to N Pages

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,8 +1,21 @@
 import { gql } from 'graphql-request'
 
 export const GET_PAGES = gql`
-  query getPages {
-    pages {
+  query getPages($first: Int) {
+    pages(first: $first) {
+      id
+      heading
+      slug
+      body {
+        html
+      }
+    }
+  }
+`
+
+export const GET_PAGE_BY_SLUG = gql`
+  query getPageBySlug($slug: String!) {
+    page(where: { slug: $slug }) {
       id
       heading
       slug
