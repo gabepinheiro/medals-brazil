b79288786dbccb9af6c5f3bc187fd22579a94531
Gabriel Pinheiro <gabrielpsilvas@live.com>
2021-08-09 16:10:31 -0300

    Write first query and use getStaticProps

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,7 +1,6 @@
 import { GraphQLClient } from 'graphql-request'
 
 const endpoint = process.env.GRAPHQL_HOST || ''
-;('https://api-us-east-1.graphcms.com/v2/cks4qyqmb05c201yx80qze8gd/master')
 
 const client = new GraphQLClient(endpoint, {
   headers: {
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,20 @@
+import client from 'graphql/client'
+import { GET_PAGES } from 'graphql/queries'
 import AboutTemplate from 'templates/About'
 
-const About = () => <AboutTemplate />
+export default function AboutPage() {
+  return <AboutTemplate />
+}
 
-export default About
+export const getStaticProps = async () => {
+  const { pages } = await client.request(GET_PAGES)
+  console.log(pages)
+  return {
+    props: {}
+  }
+}
+
+// getStaticPaths -> serve para gerar as urls em build time /about /trip/petropolis
+// gerStaticProps -> serve para buscar dados da página (props) - build time - estatico
+// getServerSideProps -> serve para buscar dados da página (props) - runtime - toda a requisição (bundle fica no server)
+// getInitialProps -> serve para buscar dados da página (props) - runtime - toda a requisição (bundle também vem para o client) - hydrate
