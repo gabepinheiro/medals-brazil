1fce34ae99f2de5f8b00758fd123c0f04e7a3e76
Gabriel Pinheiro <gabrielpsilvas@live.com>
2021-08-10 20:45:57 -0300

    Add Wrapper

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -6,12 +6,16 @@ import { MapProps } from 'components/Map'
 
 const Map = dynamic(() => import('components/Map'), { ssr: false })
 
+import * as S from './styles'
+
 export default function HomeTemplate({ places }: MapProps) {
   return (
     <>
-      <LinkWrapper href="/about">
-        <InfoOutline size={32} aria-label="About" />
-      </LinkWrapper>
+      <S.Wrapper>
+        <LinkWrapper href="/about">
+          <InfoOutline size={32} aria-label="About" />
+        </LinkWrapper>
+      </S.Wrapper>
       <Map places={places} />
     </>
   )
